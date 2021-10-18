dbc6baac4a4ceec0e05e19517a5277a40fcf5110
dwedigital <dave@dwedigital.com>
2021-10-18 14:37:33 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace times_tables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
