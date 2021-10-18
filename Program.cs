84a5966679a2ab2d4ff4410bfa67f7bb39cc117a
dwedigital <dave@dwedigital.com>
2021-10-18 15:08:13 +0100

    added while loop for logic and functions

--- Program.cs
+++ Program.cs
@@ -6,7 +6,54 @@ namespace times_tables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                 Console.WriteLine("What times table would you like (type 'exit' to quit)?");
+
+            string input = Console.ReadLine();
+            try
+            {
+                TimesTable(Convert.ToInt16(input));
+
+            }
+            catch (FormatException)
+            {
+                if (input.ToLower() =="all")
+                {
+                    AllTimesTables();
+                    
+                }
+                else if (input.ToLower() == "exit")
+                {
+                    break;
+                }else{
+                    Console.WriteLine("Sorry I have no understanding of that");
+                }
         }
+            }
+            
+
+        static void TimesTable(int num)
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"{i} x {num} = {i * num}");
+            }
+        }
+
+        static void AllTimesTables()
+        {
+            for (int x = 1; x <= 12; x++)
+            {
+                Console.WriteLine($"{x} Times Table");
+                for (int y = 1; y <= 12; y++)
+                {
+                    Console.WriteLine($"\t{y} x {x} = {x * y}");
+                }
+            }
+
+        }
+
     }
-}
+    }
+    }
